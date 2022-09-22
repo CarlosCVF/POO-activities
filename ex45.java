9757d187c64976af23cd4edee4bcb0c4b566c48c
CarlosCVF <113920027+CarlosCVF@users.noreply.github.com>
2022-09-21 22:12:46 -0300

    Add files via upload

---
+++ ex45.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class ex45 {
+
+	public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
+	/*Entrar via teclado com “N” valores quaisquer. O valor “N” (que representa a quantidade de números) será digitado, deverá ser positivo, mas menor que vinte. Caso a quantidade não satisfaça a restrição,
+	 enviar mensagem de erro e solicitar o valor novamente. Após a digitação dos “N” valores, exibir:
+	 a) O maior valor;
+	 b) O menor valor;
+	 c) A soma dos valores;
+ 	 d) A média aritmética dos valores;
+	 e) A porcentagem de valores que são positivos;
+	 f) A porcentagem de valores negativos;*/
+        
+        int N,aux=0, aux2=0, soma=0, aux3=0;
+        int[] a = new int[N];
+        
+        do 
+        {
+        System.out.printf("Digite o valor de N: ");
+        N = ler.nextInt();
+        if(N < 0 || N>20)
+        {
+        	System.out.println("Número inválido troxa!!");
+        }
+        }while(N < 0 || N > 20);
+        for(int i=0; i < N;i++)
+        {
+        	System.out.print("Digite um valor: ");
+        	a [i] = ler.nextInt();
+        	if(a[i] > aux)
+        	{
+        		aux = a[i];
+        	}
+        	soma += a[i];
+        	if(i==0)
+        		aux2 = a[i];
+        	if(a[i]<aux2)
+        		aux2 = a[i];
+        }
+        
+	}
+}
