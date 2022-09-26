53e8ac084a1fb665d65dc35be399fce98b4508bc
CarlosCVF <113920027+CarlosCVF@users.noreply.github.com>
2022-09-26 16:23:53 -0300

    Update ex40.java

--- ex40.java
+++ ex40.java
@@ -6,6 +6,21 @@ public class ex40 {
 		Scanner ler = new Scanner(System.in);
 
 		/*Exibir os vinte primeiros valores da série de Bergamaschi. A série: 1, 1, 1, 3, 5, 9, 17, ...*/
+		int s, a = 1, b = 1, c = 1;
+		for(int x = 0; x < 20; x++) 
+		{
+			if(x < 3) {
+				System.out.printf("\n1");
+			}
+			else 
+			{
+				s = a + b + c;
+				System.out.printf("\n%d",(s));
+				c = b;
+				b = a;
+				a = s;
 		ler.close();
+			}
+		}
 	}
 }
