ec21dd72ff8fcff97ee5228a06994196605e2d8e
CarlosCVF <113920027+CarlosCVF@users.noreply.github.com>
2022-09-26 16:34:57 -0300

    Update ex43.java

--- ex43.java
+++ ex43.java
@@ -7,7 +7,20 @@ public class ex43 {
 		
 		/*Calcular e exibir a soma dos “N” primeiros valores da seqüência abaixo. O valor “N” será digitado, deverá ser positivo, mas menor que cinqüenta.
 		Caso o valor não satisfaça a restrição, enviar mensagem de erro e solicitar o valor novamente.*/
+		int n;
+		float a = 0, p1 = 1;
+		do {
+		System.out.printf("Informe o tamanho da sequencia (Sendo o numero positivo e menor que 50): ");
+		n = ler.nextInt();
+		}
+		while(n < 0 || n > 49);
 		
+		for(int x = 0; x < n; x++) {
+			p1 +=2*x+1;
+			a += p1/Math.pow((x+1), 3);
+		}
+		
+		System.out.printf("A soma dos primerios %d numeros da sequencia e: %.2f", n, a);
 		ler.close();
 	}
 }
